e42be49397a4482fcf196e763f7c8478be5b36fb
Nate Chen <ziranchen@yahoo.com>
2016-02-21 02:07:56 -0500

    Update pbs_script.sh

--- pbs_script.sh
+++ pbs_script.sh
@@ -26,7 +26,7 @@ MPIRUN=/usr/lib64/openmpi/bin/mpirun
 # loop over number of processors (our 4 nodes job can run up to 48)
 for p in 6 12
 do
-        for N in 3 4 8 10 13 17
+        for N in 8 10 13 15
         do
             $MPIRUN -np $p --hostfile $PBS_NODEFILE ./nqueens -t $N 3
         done
