96fc7de5f75e300d3af2a38a30c93705528cfdcc
Nate Chen <ziranchen@yahoo.com>
2016-02-21 00:25:46 -0500

    Update pbs_script.sh

--- pbs_script.sh
+++ pbs_script.sh
@@ -14,17 +14,10 @@
 
 # TODO: change this to your project directory relative to your home directory
 #       (= $HOME)
-cd $HOME/proj1/cx4220_hw1
+cd $HOME/sp16-cse6220-prog1
 
-
-# TODO:
-# for your experiments, run your program for different `n`, different master-depth `k`
-# and with different number of processors. This will allow you to properly
-# analyze the scalability behaviour of each parameter setting.
-# Also: you should time each parameter setting multiple times and then average
-#       the results to achieve a smoother and more accurate analysis.
+# hardcode MPI path
 MPIRUN=/usr/lib64/openmpi/bin/mpirun
-PROGPATH=./nqueens
 
 # set the n-queens master-depth k
 MASTER_DEPTH=4
@@ -34,14 +27,5 @@ N=14
 # loop over number of processors (our 4 nodes job can run up to 48)
 for p in 6 12 24 48
 do
-    $MPIRUN -np $p --hostfile $PBS_NODEFILE $PROGPATH -t $N $MASTER_DEPTH
+    $MPIRUN -np $p --hostfile $PBS_NODEFILE ./nqueens -t $N $MASTER_DEPTH
 done
-
-# loop over number of processors (just an example, uncomment to use the loop)
-# for p in 2 4 8 12 16 24
-# do
-#p=24 # (four sixcore processors => 24 processes total)
-#N=8 # TODO: loop over N as well!
-#OMPI_MCA_mpi_yield_when_idle=0 mpirun --hostfile $PBS_NODEFILE -np $p $EXE -t $N $MASTER_DEPTH
-# done
-
