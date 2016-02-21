8dffba1bfb1b9c092e247927901bce228533774f
Nate Chen <ziranchen@yahoo.com>
2016-02-21 15:37:17 -0500

    Update pbs_script.sh

--- pbs_script.sh
+++ pbs_script.sh
@@ -35,9 +35,9 @@ MPIRUN=/usr/lib64/openmpi/bin/mpirun
 # N=13
 # $MPIRUN -np $p --hostfile $PBS_NODEFILE ./nqueens -t $N 4
 
-for p in 30 36 48
+for N in 8 10 13 20 30
 do
-    $MPIRUN -np $p --hostfile $PBS_NODEFILE ./nqueens -t 30 10
+    $MPIRUN -np 30 --hostfile $PBS_NODEFILE ./nqueens -t $N 5
 done
         
 # for p in 12 24 30
