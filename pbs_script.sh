ffee34c9e2265bedd7f0deac734a0ad7382ce979
Nate Chen <ziranchen@yahoo.com>
2016-02-22 01:19:37 -0500

    Update pbs_script.sh

--- pbs_script.sh
+++ pbs_script.sh
@@ -8,7 +8,7 @@
 # allocate 4 of the (24 total) sixcore nodes for up to 5 minutes
 
 #PBS -q class
-#PBS -l nodes=8:sixcore
+#PBS -l nodes=4:sixcore
 #PBS -l walltime=01:00:00
 #PBS -N cse6220-nqueens
 
@@ -35,9 +35,9 @@ MPIRUN=/usr/lib64/openmpi/bin/mpirun
 # N=13
 # $MPIRUN -np $p --hostfile $PBS_NODEFILE ./nqueens -t $N 4
 
-for p in 15 20 36 48
+for N in 8 9 10 11 12 13 15
 do
-    $MPIRUN -np $p --hostfile $PBS_NODEFILE ./nqueens -t 15 5
+    $MPIRUN -np 16 --hostfile $PBS_NODEFILE ./nqueens -t $N 5
 done
         
 # for p in 12 24 30
